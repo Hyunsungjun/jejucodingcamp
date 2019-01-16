a7901b52c1749dd1c0c16ab7f9905448947ef3e7
Jun boss1 <bnmvb01@daum.net>
2019-01-16 10:19:51 +0900

    둘째 끝

--- kakao_base/jejucoding-front/CSS/website/js/index.js
+++ kakao_base/jejucoding-front/CSS/website/js/index.js
@@ -3,7 +3,9 @@
     var menuBtn = document.querySelector('.global-menu-btn');
 
     menuBtn.addEventListener('click', function() {
-        document.body.classList.toggle('menu-on');
+        // document.body.classList.toggle('menu-on');
+        // document.body.classList.add('menu-on');
+        // document.body.classList.remove('menu-on');
     });
 
 })();
