e5c2221d3f31d3a1565676381875186f9a2ed7e6
Jun boss1 <bnmvb01@daum.net>
2019-01-17 19:04:29 +0900

    장고까지

---
+++ jejucodingcampsample/mysite/main/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from .models import Cafe
+
+def index(request):
+    if request.method == "POST":
+        #print(request)
+        #print(request.POST.keys())
+        
+        keys = list(request.POST.keys())
+        keys.remove('csrfmiddlewaretoken')
+        #print(keys[0])
+        
+        cafelistobj = []
+        cafelistobjall = Cafe.objects.all()
+        #print(cafelistobj)
+        #print(type(cafelistobj))
+        #print(dir(cafelistobj))
+    
+        for key in keys:
+            for cafelist in cafelistobjall:
+                #print(cafelist['tag'], key, cafelist.tag == key)
+                cafetag = list(cafelist.tag.values())
+                print(cafetag[0]['name'], key, cafetag[0]['name'] == key)
+                if cafetag[0]['name'] == key:
+                    cafelistobj.append(cafelist)
+        print(cafelistobj)
+        return render(request, 'main/cafelist.html', {'cafelistobj':cafelistobj})
+    return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def write(request):
+    return render(request, 'main/write.html')
+
+def cafelist(request):
+    cafelistobj = Cafe.objects.all()
+    return render(request, 'main/cafelist.html', {'cafelistobj':cafelistobj})
+
+def cafedetails(request, pk):
+    cafeobj = Cafe.objects.get(pk=pk)
+    return render(request, 'main/cafedetails.html', {'cafeobj':cafeobj})
+
+# pk = 고유번호 의미로 놓은것 
+# 경도: 33.499786 위도 : 126.5286108
+#태그는 jejusi
